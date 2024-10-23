8a2fa637dbde5b45dbab67b10ae4794cfc7c36c7
Szymon <Szymon@DESKTOP-KD7PK0F>
2024-10-23 13:26:23 +0200

    #zejsciowka1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,29 @@
 class Wektor2D
 {
+    double x;
+    double y;
+
+    public:
     // Tutaj napisz implementacje klasy Wektor2D
+    Wektor2D(double x_kon = 0., double y_kon = 0.)
+    { 
+        x = x_kon;
+        y = y_kon;
+    }
+
+    void setX(double a) { x = a; }
+    void setY(double b) { y = b; }
+
+    double getX() { return x; }
+    double getY() { return y; }
 };
+
+Wektor2D operator+(Wektor2D a, Wektor2D b)
+{
+    return Wektor2D{a.getX() + b.getX(), a.getY() + b.getY()};
+}
+
+double operator*(Wektor2D a, Wektor2D b)
+{
+    return a.getX() * b.getX() + a.getY() * b.getY();
+}
